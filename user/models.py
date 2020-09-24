8ba5e9df86c3b4907634131f5fd060df84933a56
juzi <858614365@qq.com>
2020-09-24 08:48:52 +0800

    修改了user模型

--- user/models.py
+++ user/models.py
@@ -2,11 +2,28 @@ from django.db import models
 
 
 class UserInfo(models.Model):
-    nickname = models.CharField(max_length=32, verbose_name='昵称')
-    phonenum = models.IntegerField(max_length=11, verbose_name='手机号')
-    birthday = models.DateTimeField(verbose_name='生日', default=2020 - 1 - 1)
-    gender = models.BooleanField(verbose_name='性别', default='1')
-    location = models.CharField(max_length=128, verbose_name='常居地', default='中国')
+    '''User模型'''
+
+    GENDERS = (
+        ('male', '男'),
+        ('female', '女')
+    )
+
+    LOCATIONS = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('深圳', '深圳'),
+        ('成都', '成都'),
+        ('西安', '西安'),
+        ("武汉", "武汉"),
+        ("沈阳", "沈阳"),
+    )
+    nickname = models.CharField(max_length=32, db_index=True, verbose_name='昵称')
+    phonenum = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+    birthday = models.DateTimeField(default='2002-01-01', verbose_name='生日')
+    gender = models.CharField(max_length=8, choices=GENDERS, verbose_name='性别', default='男')
+    location = models.CharField(max_length=32, choices=LOCATIONS, verbose_name='常居地', default='北京')
+    avatar = models.CharField(max_length=256, default='q', verbose_name='头像')
 
     class Meta:
         db_table = 'userinfo'
