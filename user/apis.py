b2993e74413bfb6fce2295dcc9f87b4d2ade415b
OutlierWu <18500270056@163.com>
2020-09-27 19:42:08 +0800

    1. 优化了api.py代码，将response重新封装 2. 补: 上次提交了机器人，添加了假数据

--- user/apis.py
+++ user/apis.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from common.qiniu.qn_cloud import gen_token, get_res_url
+from common.http import rend_json
 from user.logics import send_msg
 from user.models import UserModel, UserConfig
 from user.serializers.account import FetchSerializer, SubmitSerializer, LoginSerializer, UserConfigSerializer
@@ -16,7 +17,7 @@ class FetchView(APIView):
         res.is_valid(raise_exception=True)
         phonenum = res.validated_data.get("phonenum")
         send_msg(phonenum)
-        return Response({"code": 0, "data": None})
+        return rend_json()
 
 
 class SubmitView(APIView):
@@ -33,7 +34,7 @@ class SubmitView(APIView):
             user = UserModel.objects.create(phonenum=phonenum, nickname=phonenum)
         request.session["uid"] = user.id
         serializer = LoginSerializer(instance=user)
-        return Response({"code": 0, "data": serializer.data})
+        return rend_json(data=serializer.data)
 
 
 class ProfileShowView(APIView):
@@ -44,7 +45,7 @@ class ProfileShowView(APIView):
         _res, _ = UserConfig.objects.get_or_create(id=userid)
         serializer = UserConfigSerializer(instance=_res)
         print(serializer.data, "--------")
-        return Response({"code": 0, "data": serializer.data})
+        return rend_json(data=serializer.data)
 
 
 class ProfileUpdateView(APIView):
@@ -62,12 +63,12 @@ class ProfileUpdateView(APIView):
             userid = request.session.get("uid")
             UserModel.objects.filter(id=userid).update(**user_serializer.validated_data)
             UserConfig.objects.update_or_create(id=userid, defaults=config_serializer.validated_data)
-            return Response({"code": 0, "data": None})
+            return rend_json()
         else:
             res = {}
             res.update(user_serializer.errors)
             res.update(config_serializer.errors)
-            return Response({"code": 1003, "data": res})
+            return rend_json(data=res, code=1003)
 
 
 class QnTokenView(APIView):
@@ -77,7 +78,7 @@ class QnTokenView(APIView):
         uid = request.session["uid"]
         filename = f"Avatar-{uid}"
         token = gen_token(uid, filename)
-        return Response({"code": 0, "data": {"token": token, "key": filename}})
+        return rend_json(data={"token": token, "key": filename})
 
 
 class QnCallbackView(APIView):
@@ -89,4 +90,4 @@ class QnCallbackView(APIView):
         print(uid, key)
         avatar_url = get_res_url(key)
         UserModel.objects.filter(id=uid).update(avatar=avatar_url)
-        return Response({"code": 0, "data": avatar_url})
+        return rend_json(data=avatar_url)
